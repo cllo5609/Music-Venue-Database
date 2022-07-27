245df1e8261aa7aa77181fe0bd8e2a6a9e122c9f
Charles Maddux <madduxc@flip1.engr.oregonstate.edu>
2022-07-26 22:10:43 -0700

    Added select functionality to all pages.  Added Insert to Rooms.

--- app-client-side/app.js
+++ app-client-side/app.js
@@ -1,64 +1,156 @@
+const util = require('util');
+require('util.promisify').shim();
+
+const fs = require('fs');
+const readFileAsync = util.promisify(fs.readFile);
 
 // Setup
+// express
 var express = require('express');
 var app = express();
-PORT = 37292;
+PORT = 2159;
 
+// express-handlebars
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');
 app.engine('.hbs', engine({extname: ".hbs"}));
 app.set('view engine', '.hbs');
 app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+// database
+var db = require('./database/db-connector')     // require the db file
 
 // Routes
 
 // Homepage
 app.get('/', (req, res) =>{
-        res.render('index');
-    });
+    res.render('index');
+});
 
 // Venue Page
 app.get('/venues', (req, res) =>{
-        res.render('venues');
+    let query_venues = "SELECT name, address, city, state, zip, phone, num_rooms, total_capacity FROM Venues;";
+    db.pool.query(query_venues, function(errors, rows, fields) {
+        res.render('venues', {data:rows})
     });
+});
 
 app.get('/venue_mod', (req, res) =>{
-        res.render('venue_mod');
-    });
+    res.render('venue_mod');
+});
 
 // Rooms Page
 app.get('/rooms', (req, res) =>{
-    res.render('rooms');
+    let query_rooms = "SELECT Rooms.name AS 'Room', capacity AS 'Capacity', Venues.name AS 'Venue', over_21 AS 'Adult' FROM Rooms INNER JOIN Venues ON Rooms.venue=Venues.venue_id;";
+    db.pool.query(query_rooms, function(errors, rows, fields) {
+        res.render('rooms', {data:rows});
+    });
+});
+
+app.post('/add-room', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+    // Capture NULL values
+    let Capacity = parseInt(data.Capacity);
+    if (isNaN(Capacity))
+    {
+        Capacity = 'NULL'
+    }
+
+    let Adult = parseInt(data.Adult);
+    if (isNaN(Adult))
+    {
+        Adult = 'NULL'
+    }
+
+    // INSERT INTO Rooms (name, venue, capacity, over_21)
+    // VALUES  ('Slice', (SELECT venue_id FROM Venues WHERE name = 'SOMA'), 20, 1)
+    // -- VALUES (:rNameInput, :rVenueSelect, :rCapacityInput, :adultVenueInput);
+    
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Rooms (name, capacity, venue, over_21) VALUES ('${data.Room}', '${Capacity}', ${data.Venue}, ${Adult})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = "SELECT Rooms.name AS 'Room', capacity AS 'Capacity', Venues.name AS 'Venue', over_21 AS 'Adult' FROM Rooms INNER JOIN Venues ON Rooms.venue=Venues.venue_id;";
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    console.log("chickens");
+                    res.send(rows);
+                }
+            })
+        }
+    })
 });
 
 // Performances Page
 app.get('/performances', (req, res) =>{
-    res.render('performances');
+    let query_performances = "SELECT Artists.name, Performances.performance_date, ticket_price, Performances.tickets_available FROM Tickets INNER JOIN Performances ON Tickets.ticket_id=Performances.ticket_id INNER JOIN Artists ON Performances.artist_id=Artists.artist_id;";
+    db.pool.query(query_performances, function(errors, rows, fields) {
+        res.render('performances', {data:rows});
+    });
 });
 
 // Artists Page
 app.get('/artists', (req, res) =>{
-    res.render('artists');
+    let query_artists = "SELECT name, email, phone, genre FROM Artists;";
+    db.pool.query(query_artists, function(errors, rows, fields) {
+        res.render('artists', {data:rows});
+    });
 });
 
 // Instruments Page
 app.get('/instruments', (req, res) =>{
-    res.render('instruments');
+    let query_instruments = "SELECT instrument_id, instrument_type FROM Instruments;";
+    db.pool.query(query_instruments, function(errors, rows, fields) {
+        res.render('instruments', {data:rows});
+    });
 });
 
 // Tickets Page
 app.get('/tickets', (req, res) =>{
-    res.render('tickets');
+    let query_tickets = "SELECT ticket_id, ticket_price FROM Tickets;";
+    db.pool.query(query_tickets, function(errors, rows, fields) {
+        res.render('tickets', {data:rows});
+    });
 });
 
 // Ticket Invoices Page
 app.get('/ticket_invoices', (req, res) =>{
-    res.render('ticket_invoices');
+    let query_ticket_invoices = "SELECT Ticket_Invoices.event_id AS 'Invoice', Artists.name AS 'Performing', Venues.name AS 'Location', Rooms.name AS 'Room', Performances.performance_date AS 'Date', ticket_price AS 'Price' FROM Tickets INNER JOIN Ticket_Invoices ON Tickets.ticket_id=Ticket_Invoices.ticket_id INNER JOIN Performances ON Ticket_Invoices.performance_id=Performances.performance_id INNER JOIN Artists ON Performances.artist_id=Artists.artist_id INNER JOIN Rooms ON Performances.room_id=Rooms.room_id INNER JOIN Venues ON Rooms.venue=Venues.venue_id;";
+    db.pool.query(query_ticket_invoices, function(errors, rows, fields) {
+        res.render('ticket_invoices', {data:rows});
+    });
 });
 
 // Artist Needs Instruments Page
 app.get('/artist_needs_ins', (req, res) =>{
-    res.render('artist_needs_ins');
+    let query_artist_needs_ins = "SELECT Artists.name AS 'Band', Instruments.instrument_type AS 'Gear' FROM Artists INNER JOIN Artist_Needs_Instruments ON Artists.artist_id=Artist_Needs_Instruments.artist_id INNER JOIN Instruments ON Artist_Needs_Instruments.instrument_id=Instruments.instrument_id;";
+    db.pool.query(query_artist_needs_ins, function(errors, rows, fields) {
+        res.render('artist_needs_ins', {data:rows});
+    });
 });
 
 // Listener
---
+++ app-client-side/database/Group_56_DDL.sql
@@ -0,0 +1,175 @@
+/*  Authors:    Charles D. Maddux
+                Clinton Lohr
+    Assignment: Final Project - create database tables
+    Date:       10 July 2022
+    Revision:   0.3.1
+    Rev Date:   18 July 2022
+*/
+
+-- set foreign key check and turn off autocommit
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT=0;
+
+-- create tables
+CREATE OR REPLACE TABLE Venues (
+    venue_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    num_rooms INT NOT NULL,
+    total_capacity INT NOT NULL,
+    phone VARCHAR(12) NOT NULL,
+    address VARCHAR(100),
+    city VARCHAR(100),
+    state VARCHAR(2),
+    zip INT(5),
+    PRIMARY KEY (venue_id)
+);
+
+CREATE OR REPLACE TABLE Rooms (
+    room_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    venue INT NOT NULL,
+    capacity INT NOT NULL,
+    over_21 TINYINT(1) DEFAULT 0,
+    PRIMARY KEY (room_id),
+    FOREIGN KEY (venue) REFERENCES Venues (venue_id)
+    ON DELETE CASCADE
+);
+
+CREATE OR REPLACE TABLE Artists (
+    artist_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(12) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    genre VARCHAR(50),
+    PRIMARY KEY (artist_id)
+);
+
+CREATE OR REPLACE TABLE Performances (
+    performance_id INT NOT NULL AUTO_INCREMENT,
+    artist_id INT NOT NULL,
+    room_id INT NOT NULL,
+    performance_date DATETIME,
+    ticket_id INT NOT NULL,
+    tickets_available INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (performance_id),
+    FOREIGN KEY (artist_id) REFERENCES Artists (artist_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (room_id) REFERENCES Rooms (room_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (ticket_id) REFERENCES Tickets (ticket_id)
+);
+
+CREATE OR REPLACE TABLE Instruments (
+    instrument_id INT NOT NULL AUTO_INCREMENT,
+    instrument_type VARCHAR(50),
+    PRIMARY KEY (instrument_id)
+);
+
+CREATE OR REPLACE TABLE Tickets (
+    ticket_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    ticket_price DECIMAL(5,2),
+    PRIMARY KEY (ticket_id)
+);
+
+CREATE OR REPLACE TABLE Ticket_Invoices (
+    event_id INT NOT NULL AUTO_INCREMENT,
+    performance_id INT NOT NULL,
+    ticket_id INT NOT NULL,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (performance_id) REFERENCES Performances (performance_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (ticket_id) REFERENCES Tickets (ticket_id)
+    ON DELETE CASCADE
+);
+
+CREATE OR REPLACE TABLE Artist_Needs_Instruments (
+    rental_id INT NOT NULL AUTO_INCREMENT,
+    artist_id INT NOT NULL,
+    instrument_id INT NOT NULL,
+    PRIMARY KEY (rental_id),
+    FOREIGN KEY (artist_id) REFERENCES Artists (artist_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (instrument_id) REFERENCES Instruments (instrument_id)
+    ON DELETE CASCADE
+);
+
+-- #########################################################################################
+-- insert statements
+INSERT INTO Venues (name, address, city, state, zip, phone, num_rooms, total_capacity)
+VALUES  ('Broken Spoke', '3201 S. Lamar Blvd.', 'Austin', 'TX', '78704', 
+        '512-442-6189', 3, 0),
+        ('SOMA', '3350 Sports Arena Blvd.', "San Diego", 'CA', NULL, 
+        '619-226-7662', 2, 0),
+        ('The Crocodile', '2505 1st Ave.', 'Seattle', 'WA', '98121',
+        '206-441-5555', 3, 0);
+
+INSERT INTO Rooms (name, venue, capacity, over_21)
+VALUES  ('Pony Room', (SELECT venue_id FROM Venues WHERE name = 'Broken Spoke'), 75, 0),
+        ('Oregon Trail', (SELECT venue_id FROM Venues WHERE name = 'Broken Spoke'), 50, 1),
+        ('The Clydesdale', (SELECT venue_id FROM Venues WHERE name = 'Broken Spoke'), 125, 1),
+        ('Calamity', (SELECT venue_id FROM Venues WHERE name = 'SOMA'), 45, 1),
+        ('Mind Freeze', (SELECT venue_id FROM Venues WHERE name = 'SOMA'), 65, 1),
+        ('Nevermind', (SELECT venue_id FROM Venues WHERE name = 'The Crocodile'), 100, 0),
+        ('Bleach', (SELECT venue_id FROM Venues WHERE name = 'The Crocodile'), 120, 0),
+        ('In Utero', (SELECT venue_id FROM Venues WHERE name = 'The Crocodile'), 90, 0);
+
+-- Auto-populate total capacity AFTER both Venues & Rooms tables are populated
+UPDATE Venues
+        SET total_capacity = (SELECT SUM(capacity) FROM Rooms WHERE venue = 1 GROUP BY venue) WHERE venue_id = 1;
+UPDATE Venues
+        SET total_capacity = (SELECT SUM(capacity) FROM Rooms WHERE venue = 2 GROUP BY venue) WHERE venue_id = 2;
+UPDATE Venues
+        SET total_capacity = (SELECT SUM(capacity) FROM Rooms WHERE venue = 3 GROUP BY venue) WHERE venue_id = 3;
+
+INSERT INTO Artists (name, email, phone, genre)
+VALUES  ('Modest Mouse', 'MM@gmail.com', '123-456-7890', 'indie rock'),
+        ('System of a Down', 'sysdown@gmail.com', '234-567-8901', 'progressive metal'),
+        ('James Taylor', 'jtaylor@gmail.com', '345-678-9012', 'folk'),
+        ('Stitched Up Heart', 'goth_girl@tragic.com', '800-555-5555', 'Emo');
+
+INSERT INTO Tickets (ticket_price)
+VALUES  (50.00),
+        (60.00),
+        (70.00),
+        (80.00),
+        (90.00),
+        (100.00);
+
+INSERT INTO Performances (performance_date, artist_id, ticket_id, room_id, tickets_available)
+VALUES  ('2022-07-23', (SELECT artist_id FROM Artists WHERE name = 'Modest Mouse'), 
+        (SELECT ticket_id FROM Tickets WHERE ticket_price = 60.00), 
+        (SELECT room_id FROM Rooms WHERE name = 'Pony Room'), 45),
+
+        ('2022-07-30', (SELECT artist_id FROM Artists WHERE name = 'System of a Down'), 
+        (SELECT ticket_id FROM Tickets WHERE ticket_price = 70.00), 
+        (SELECT room_id FROM Rooms WHERE name = 'Oregon Trail'), 10),
+
+        ('2022-08-06', (SELECT artist_id FROM Artists WHERE name = 'James Taylor'), 
+        (SELECT ticket_id FROM Tickets WHERE ticket_price = 90.00), 
+        (SELECT room_id FROM Rooms WHERE name = 'The Clydesdale'), 99);
+
+INSERT INTO Instruments (instrument_type)
+VALUES  ('electric guitar'),
+        ('acoustic guitar'),
+        ('bass guitar'),
+        ('drum set');
+
+INSERT INTO Ticket_Invoices (performance_id, ticket_id)
+VALUES  ((SELECT performance_id FROM Performances WHERE performance_date = '2022-07-23'), 
+        (SELECT ticket_id FROM Tickets WHERE ticket_price = '70.00')),
+        ((SELECT performance_id FROM Performances WHERE performance_date = '2022-07-30'), 
+        (SELECT ticket_id FROM Tickets WHERE ticket_price = '60.00')),
+        ((SELECT performance_id FROM Performances WHERE performance_date = '2022-08-06'), 
+        (SELECT ticket_id FROM Tickets WHERE ticket_price = '90.00'));
+
+INSERT INTO Artist_Needs_Instruments (instrument_id, artist_id)
+VALUES  ((SELECT instrument_id FROM Instruments WHERE instrument_type = 'electric guitar'), 
+        (SELECT artist_id FROM Artists WHERE name = 'James Taylor')),
+        ((SELECT instrument_id FROM Instruments WHERE instrument_type = 'bass guitar'), 
+        (SELECT artist_id FROM Artists WHERE name = 'System of a Down')),
+        ((SELECT instrument_id FROM Instruments WHERE instrument_type = 'electric guitar'), 
+        (SELECT artist_id FROM Artists WHERE name = 'Modest Mouse')); 
+        
+-- turn on checks and commit
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
