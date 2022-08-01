9ebce572f40a5a535f5df1691769bf6bd18a33c7
Clint Lohr <55637731+cllo5609@users.noreply.github.com>
2022-07-31 21:00:51 -0600

    Create mod_artist.js

---
+++ app-client-side/public/js/mod_artist.js
@@ -0,0 +1,158 @@
+// SHOW ADD FORM
+function displayAddForm() {
+    let x = document.getElementById("show-add");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+}
+
+// SHOW UPDATE FORM
+function displayUpdateForm() {
+    let x = document.getElementById("show-update");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+}
+
+let addArtistForm = document.getElementById('add-artist-form');
+
+addArtistForm.addEventListener("submit", function (e){
+
+    e.preventDefault();
+
+    // get form fields
+
+    let inputName = document.getElementById("add-artist");
+    let inputEmail = document.getElementById("add-email");
+    let inputPhone = document.getElementById("add-phone");
+    let inputGenre = document.getElementById("add-genre");
+
+    // get values
+    let nameValue = inputName.value;
+    let emailValue = inputEmail.value;
+    let phoneValue = inputPhone.value;
+    let genreValue = inputGenre.value;
+
+
+    // put data in js object
+    let data = {
+        aname: nameValue,
+        aemail: emailValue,
+        aphone: phoneValue,
+        agenre: genreValue
+    }
+
+    // AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-artist", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // resolve AJAX request
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            //addRowToTable(xhttp.response);
+
+            // clear input fields
+            inputName.value = '';
+            inputEmail.value = '';
+            inputPhone.value = '';
+            inputGenre.value = '';
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 400) {
+            console.log("ERROR: There was a problem with the input")
+        }
+    }
+    xhttp.send(JSON.stringify(data));
+    location.reload()
+})
+
+
+// DELETE
+
+function deleteArtist(artistID) {
+
+    let data = {
+        id: artistID
+    };
+
+    // AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete-artist", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Resolve request
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    xhttp.send(JSON.stringify(data));
+    location.reload();
+}
+
+
+// UPDATE
+
+let updateArtistForm = document.getElementById('update-artist-form');
+
+updateArtistForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    // Get form fields
+    let inputID = document.getElementById("input-id");
+    let inputName = document.getElementById("update-name");
+    let inputEmail = document.getElementById("update-email");
+    let inputPhone = document.getElementById("update-phone");
+    let inputGenre = document.getElementById("update-genre");
+
+    // Get values from fields
+    let idValue = inputID.value;
+    let nameValue = inputName.value;
+    let emailValue = inputEmail.value;
+    let phoneValue = inputPhone.value;
+    let genreValue = inputGenre.value;
+
+
+    // Create javascript object with values
+    let data = {
+        aid: idValue,
+        aname: nameValue,
+        aemail: emailValue,
+        aphone: phoneValue,
+        agenre: genreValue
+    }
+
+    // AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("PUT", "/update-artist", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Resolve request
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            inputName.value = '';
+            inputEmail.value = '';
+            inputPhone.value = '';
+            inputGenre.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request
+    xhttp.send(JSON.stringify(data));
+    location.reload();
+})
